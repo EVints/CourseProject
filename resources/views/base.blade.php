5061d643c33bd50471fd88f4eb3f6954ad37176a
EVints <vintsea@gmail.com>
2022-03-13 12:55:37 +0300

    init add migration

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -107,6 +107,7 @@ Licence URI: https://www.os-templates.com/template-terms
         <!-- ################################################################################################ -->
         <div id="pageintro" class="hoc clear">
             <!-- ################################################################################################ -->
+            @section('pageIntro')
             <article>
                 <h3 class="heading">Finibus dictum lobortis</h3>
                 <p>Libero sed ullamcorper nibh dignissim et curabitur cursus scelerisque metus sit amet sodales justo auctor sit amet proin quis lacus non nisi.</p>
@@ -117,6 +118,7 @@ Licence URI: https://www.os-templates.com/template-terms
                     </ul>
                 </footer>
             </article>
+            @endsection
             <!-- ################################################################################################ -->
         </div>
         <!-- ################################################################################################ -->
@@ -127,74 +129,10 @@ Licence URI: https://www.os-templates.com/template-terms
     <!-- ################################################################################################ -->
     <div class="wrapper row3">
         <main class="hoc container clear">
-            <!-- main body -->
-            <!-- ################################################################################################ -->
-            <section id="introblocks">
-                <ul class="nospace group btmspace-80">
-                    <li class="one_third first">
-                        <figure><a class="imgover" href="#"><img src="/images/demo/348x261.png" alt=""></a>
-                            <figcaption>
-                                <h6 class="heading">Viverra congue</h6>
-                                <p>Aenean metus purus consectetur ac sagittis in malesuada quis nunc ut sed risus nulla etiam gravida velit.</p>
-                            </figcaption>
-                        </figure>
-                    </li>
-                    <li class="one_third">
-                        <figure><a class="imgover" href="#"><img src="/images/demo/348x261.png" alt=""></a>
-                            <figcaption>
-                                <h6 class="heading">Libero sodales</h6>
-                                <p>Nec tincidunt maximus ex orci sollicitudin metus ut lacinia ligula nisi vel neque sed non quam eleifend.</p>
-                            </figcaption>
-                        </figure>
-                    </li>
-                    <li class="one_third">
-                        <figure><a class="imgover" href="#"><img src="/images/demo/348x261.png" alt=""></a>
-                            <figcaption>
-                                <h6 class="heading">Laoreet tortor</h6>
-                                <p>Pharetra etiam ut nisi non mi scelerisque consectetur maecenas vel elementum lectus cras maximus finibus.</p>
-                            </figcaption>
-                        </figure>
-                    </li>
-                </ul>
-            </section>
-            <!-- ################################################################################################ -->
-            <hr class="btmspace-80">
-            <!-- ################################################################################################ -->
-            <section id="overview">
-                <div class="sectiontitle">
-                    <p class="nospace font-xs">Integer facilisis enim sollicitudin</p>
-                    <p class="heading underline font-x2">Dolor ut pellentesque</p>
-                </div>
-                <ul class="nospace group btmspace-80">
-                    <li class="one_third">
-                        <article>
-                            <div class="clear"><a href="#"><i class="fas fa-tape"></i></a>
-                                <h6 class="heading">Efficitur auctor</h6>
-                            </div>
-                            <p>Cras dui ac pretium egestas nunc maecenas scelerisque leo eget enim interdum at vulputate lorem pretium in.</p>
-                        </article>
-                    </li>
-                    <li class="one_third">
-                        <article>
-                            <div class="clear"><a href="#"><i class="fas fa-table-tennis"></i></a>
-                                <h6 class="heading">Tempor aliquam</h6>
-                            </div>
-                            <p>Sagittis arcu a magna eget cursus lacus consectetur proin imperdiet bibendum elit id molestie ipsum ut tellus.</p>
-                        </article>
-                    </li>
-                    <li class="one_third">
-                        <article>
-                            <div class="clear"><a href="#"><i class="fas fa-people-carry"></i></a>
-                                <h6 class="heading">Metus convallis</h6>
-                            </div>
-                            <p>Orci blandit ac mauris ac gravida maximus nulla curabitur convallis massa sed urna placerat sed tempor velit.</p>
-                        </article>
-                    </li>
-                </ul>
-                <footer class="center"><a class="btn" href="#">Mattis vestibulum</a></footer>
-            </section>
-            <!-- ################################################################################################ -->
-            <!-- / main body -->
+
+            @yield('mainBody')
+
+
             <div class="clear"></div>
         </main>
     </div>
