0c200c80ac6b7af3d84b1dd63975483031d33081
EVints <vintsea@gmail.com>
2022-03-13 12:56:07 +0300

    init add migration

---
+++ resources/views/advert/create.blade.php
@@ -0,0 +1,23 @@
+@extends('base')
+
+@section('pageIntro')
+@endsection
+
+@section('mainBody')
+
+<section id="introblocks">
+
+    <form action="" method="post">
+
+        <input type="datetime-local" name="from" id="">
+        <input type="datetime-local" name="to" id="">
+        <input type="text" name="point_meet" id="">
+        <input type="text" name="point_meet" id="">
+
+        <input type="email" name="" id="">
+        <input type="button" value="">
+    </form>
+
+</section>
+
+@endsection
